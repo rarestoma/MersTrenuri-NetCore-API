4c9d91362ee9c430df789bcbf20e26e1ff282be1
rarestoma <tomairares@gmail.com>
2019-12-19 16:19:51 +0200

    added files

---
+++ DAW.Domain.EF/HotelDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DAW.Domain.EF.ModelConfigurations;
+using DAW.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAW.Domain.EF
+{
+    public class HotelDbContext : DbContext
+    {
+        public HotelDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerEntityConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeEntityConfiguration());
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
---
+++ DAW.Domain/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using DAW.Domain.Models.BaseModels;
+using System;
+
+namespace DAW.Domain.Models
+{
+    public class Booking : BaseEntity
+    {
+        public int CustomerId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MyProperty { get; set; }
+        public float TotalPrice { get; set; }
+        public int PersonCount { get; set; }
+        public string Status { get; set; }
+    }
+}
